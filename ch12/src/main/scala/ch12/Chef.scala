a2cd27cefe1a4d5296bbee3ffba9a7dd7fde682c
slasch <slavaschmidt@gmx.de>
2018-07-14 12:12:45 +0200

    ch12: Supervision

--- ch12/src/main/scala/ch12/Chef.scala
+++ ch12/src/main/scala/ch12/Chef.scala
@@ -1,40 +1,49 @@
 package ch12
 
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, DispatcherSelector}
+import akka.actor.typed.{ActorRef, Behavior, DispatcherSelector}
 import ch12.Bakery.{Groceries, Pastry}
 import ch12.Manager.ReceivePastry
 
 object Chef {
   sealed trait Command
 
-  final case class Mix(g: Groceries, manager: ActorRef[Manager.Command]) extends Command
+  final case class Mix(g: Groceries, manager: ActorRef[Manager.Command])
+      extends Command
   final case class Collect(p: Pastry, mixer: ActorRef[Mixer.Mix])
       extends Command
+  final case class BrokenMixer(mixer: ActorRef[Mixer.Mix]) extends Command
 
   def idle: Behaviors.Receive[Command] = Behaviors.receivePartial[Command] {
-    case (context, mix @ Mix(Groceries(eggs, flour, sugar, chocolate), manager)) =>
+    case (context,
+          mix @ Mix(Groceries(eggs, flour, sugar, chocolate), manager)) =>
       val mixers = for (i <- 1 to eggs)
         yield
           context.spawn(Mixer.mix,
                         s"Mixer_$i",
                         DispatcherSelector.fromConfig("mixers-dispatcher"))
+      mixers.foreach(mixer => context.watchWith(mixer, BrokenMixer(mixer)))
       mixers.foreach(_ ! Mixer.Mix(mix.g, context.self))
       mixing(mixers.toSet, 0, manager)
   }
 
   def mixing(mixers: Set[ActorRef[Mixer.Mix]],
-             collected: Int, manager: ActorRef[Manager.Command]): Behaviors.Receive[Command] =
-    Behaviors.receivePartial {
-      case (context, Collect(pastry, mixer)) =>
-        val mixersToGo = mixers - mixer
-        val pastryBuf = collected + pastry.weight
-        context.stop(mixer)
-        if (mixersToGo.isEmpty) {
-          manager ! ReceivePastry(Pastry(pastryBuf))
-          idle
-        } else {
-          mixing(mixersToGo, pastryBuf, manager)
-        }
-    }
+             collected: Int,
+             manager: ActorRef[Manager.Command]): Behavior[Command] =
+      Behaviors.receivePartial {
+        case (context, Collect(pastry, mixer)) =>
+          val mixersToGo = mixers - mixer
+          val pastryBuf = collected + pastry.weight
+          context.stop(mixer)
+          if (mixersToGo.isEmpty) {
+            manager ! ReceivePastry(Pastry(pastryBuf))
+            idle
+          } else {
+            mixing(mixersToGo, pastryBuf, manager)
+          }
+        case (context, BrokenMixer(m)) =>
+          context.log.warning("Broken mixer detected {}", m)
+          context.self ! Collect(Pastry(0), m)
+          Behaviors.same
+      }
 }
--- ch12/src/main/scala/ch12/Mixer.scala
+++ ch12/src/main/scala/ch12/Mixer.scala
@@ -1,21 +1,34 @@
 package ch12
 
-import akka.actor.typed.ActorRef
+import akka.actor.typed.{ActorRef, Behavior, SupervisorStrategy}
 import akka.actor.typed.scaladsl.Behaviors
 import ch12.Bakery.{Groceries, Pastry}
 import ch12.Chef.Collect
 
+import scala.util.Random
+
 object Mixer {
+  class MotorOverheatException extends Exception
+  class SlowRotationSpeedException extends Exception
+  class StrongVibrationException extends Exception
+
   final case class Mix(groceries: Groceries, sender: ActorRef[Collect])
-  def mix = Behaviors.receive[Mix] {
+
+  def mix: Behavior[Mix] = Behaviors.receive[Mix] {
     case (ctx, Mix(Groceries(eggs, flour, sugar, chocolate), sender)) =>
+      if (Random.nextBoolean()) throw new MotorOverheatException
       Thread.sleep(3000)
       sender ! Collect(Pastry(eggs * 50 + flour + sugar + chocolate), ctx.self)
       Behaviors.stopped
   }
+
+  val controlledMix: Behavior[Mix] =
+    Behaviors
+      .supervise(
+        Behaviors
+          .supervise(Behaviors
+            .supervise(mix)
+            .onFailure[MotorOverheatException](SupervisorStrategy.stop))
+          .onFailure[SlowRotationSpeedException](SupervisorStrategy.restart))
+      .onFailure[StrongVibrationException](SupervisorStrategy.resume)
 }
-/*
-  class MotorOverheatException extends Exception
-  class SlowRotationSpeedException extends Exception
-  class StrongVibrationException extends Exception
- */
