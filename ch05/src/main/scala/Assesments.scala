016606cf7287bcd2452247f2b9c087ef451ba9cb
slasch <slavaschmidt@gmx.de>
2018-09-12 09:05:21 +0200

    Ch05: code examples

---
+++ ch05/src/main/scala/Assesments.scala
@@ -0,0 +1,39 @@
+import java.util.UUID
+
+import org.scalacheck.{Arbitrary, Gen, Prop}
+import org.scalacheck.Prop.forAll
+
+object Assesments extends App {
+  def invariant[T: Ordering: Arbitrary]: Prop =
+    forAll((l: List[T]) => l.sorted.length == l.length)
+
+  invariant[Long].check
+  invariant[String].check
+
+  def idempotent[T: Ordering: Arbitrary]: Prop =
+    forAll((l: List[T]) => l.sorted.sorted == l.sorted)
+
+  idempotent[Long].check
+  idempotent[String].check
+
+  def inductive[T: Ordering: Arbitrary]: Prop = {
+    def ordered(l: List[T]): Boolean =
+      (l.length < 2) ||
+        (ordered(l.tail) && implicitly[Ordering[T]].lteq(l.head, l.tail.head))
+    forAll((l: List[T]) => ordered(l.sorted))
+  }
+
+  inductive[Int].check
+  inductive[String].check
+
+
+  val genListListInt = Gen.listOf(Gen.listOf(Gen.posNum[Int]))
+  genListListInt.sample
+
+  val pairGen = for {
+    uuid <- Gen.uuid
+    function0 <- Gen.function0(Gen.asciiStr)
+  } yield (uuid, function0)
+
+  val mapGen = Gen.mapOf(pairGen)
+}
---
+++ ch05/src/main/scala/Generators.scala
@@ -0,0 +1,72 @@
+import org.scalacheck.Prop._
+import org.scalacheck.{Gen, Prop}
+
+object Generators {
+
+  val morgans: Prop = forAll { (a: Boolean, b: Boolean) =>
+    collect(s"$a $b")(!(a & b) == (!a | !b))
+  }
+  morgans.check
+
+/*
+
+  TODO uncomment with scala 2.13
+
+  def literalGen[T <: Singleton](t: T): Gen[T] = Gen.const(t)
+
+  implicit val myGen: Arbitrary[42] = Arbitrary(literalGen(42))
+
+  val literalProp = forAll((_: 42) == 42).check
+*/
+
+  sealed trait Rank
+  case class SymRank(s: Char) extends Rank {
+    override def toString: String = s.toString
+  }
+  case class NumRank(n: Int) extends Rank {
+    override def toString: String = n.toString
+  }
+  case class Card(suit: Char, rank: Rank) {
+    override def toString: String = s"$suit $rank"
+  }
+
+  val suits: Gen[Char] = Gen.oneOf('♡', '♢', '♤', '♧')
+  val numbers: Gen[NumRank] = Gen.choose(2, 10).map(NumRank)
+  val symbols: Gen[SymRank] = Gen.oneOf('A', 'K', 'Q', 'J').map(SymRank)
+
+  val full: Gen[Card] = for {
+    suit <- suits
+    rank <- Gen.frequency((9, numbers), (4, symbols))
+  } yield Card(suit, rank)
+
+  val piquetBad: Gen[Card] = full.suchThat {
+    case Card(_, _: SymRank) => true
+    case Card(_, NumRank(n)) => n > 5
+  }
+
+
+  forAll(full) { card =>
+    Prop.collect(card)(true)
+  }.check
+
+  val piquetNumbers: Gen[NumRank] = Gen.choose(6, 10).map(NumRank)
+
+  val piquet: Gen[Card] = for {
+    suit <- suits
+    rank <- Gen.frequency((5, piquetNumbers), (4, symbols))
+  } yield Card(suit, rank)
+
+  forAll(piquet) { card =>
+    Prop.collect(card)(true)
+  }.check
+
+
+  val handOfCards1: Gen[List[Card]] = Gen.listOfN(6, piquet)
+
+  val handOfCards: Gen[Set[Card]] = Gen.containerOfN[Set, Card](6, piquet).retryUntil(_.size == 6)
+
+  forAll(handOfCards) { hand =>
+    Prop.collect(hand.mkString(","))(true)
+  }.check
+
+}
