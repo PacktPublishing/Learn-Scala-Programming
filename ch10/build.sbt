4b1a72e45274a84a34cad97dd1da08c3e7ebe732
slasch <slavaschmidt@gmx.de>
2018-10-18 08:37:22 +0200

    Ch10: WIP, chapters code

--- ch10/build.sbt
+++ ch10/build.sbt
@@ -2,4 +2,13 @@ organization := "packt"
 version := "1.0-SNAPSHOT"
 scalaVersion := "2.13.0-M5"
 
-scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked")
+lazy val ch08 = RootProject(file("../ch08"))
+
+lazy val ch09 = RootProject(file("../ch09"))
+
+lazy val ch10 = project.in(file(".")).settings(
+  scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked"),
+  libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.0" withSources() withJavadoc(),
+  addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.8")
+).dependsOn(ch08, ch09)
+
---
+++ ch10/src/main/scala/ch10/TransformerStacks.scala
@@ -0,0 +1,38 @@
+package ch10
+
+import ch10.Ch10._
+
+import scala.concurrent.Future
+import scala.language.higherKinds
+import scala.concurrent.ExecutionContext.Implicits.global
+import ch10.Transformers._
+import TransformerStacks._
+
+object TransformerStacks {
+
+  type Inner[A] = EitherT[Future, String, A]
+  type Outer[F[_], A] = OptionT[F, A]
+  type Stack[A] = Outer[Inner, A] // OptionT[EitherT[Future, String, A], A]
+
+}
+
+object Ch10OptionTEitherTFutureFishing extends FishingApi[Stack[?]] with App {
+
+  val buyBateImpl: String => Future[Bate] = ???
+  val castLineImpl: Bate => Either[String, Line] = ???
+  val hookFishImpl: Line => Future[Fish] = ???
+
+  override val castLine: Bate => Stack[Line] =
+    (bate: Bate) => new EitherT(Future.successful(castLineImpl(bate).map(Option.apply)))
+
+  override val buyBate: String => Stack[Bate] =
+    (name: String) => new EitherT(buyBateImpl(name).map(l => Right(Option(l)): Either[String, Option[Bate]]))
+
+  override val hookFish: Line => Stack[Fish] =
+    (line: Line) => new EitherT(hookFishImpl(line).map(l => Right(Option(l)): Either[String, Option[Fish]]))
+
+  val input = optionTunit[Inner, String]("Crankbait")
+  val outerResult: Inner[Option[Fish]] = goFishing(input).value
+  val innerResult: Future[Either[String, Option[Fish]]] = outerResult.value
+
+}
